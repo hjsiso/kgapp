b3f8c7caaae2522587ce19e90bf6bfabeb08e38b
Hector <hjsiso@gmail.com>
2015-09-16 18:03:58 -0430

    icons fab dial + nav register

--- client/home/controllers/homeLoginopt.js
+++ client/home/controllers/homeLoginopt.js
@@ -1,5 +1,9 @@
 angular.module("kgapp").controller("LoginOptCtrl", ['$meteor', '$state','$mdToast',
   function ($meteor, $state, $mdToast) {
+  	var vm = this;
 
+  	vm.access = function () {
+  		$state.go('login');
+  	};
    }
 ]);
