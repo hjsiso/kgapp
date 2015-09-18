e8e0fd5237da86fcab903a5ae23c2178230574ae
Hector <hjsiso@gmail.com>
2015-09-17 21:35:50 -0430

    materialize 1

--- client/lib/app.js
+++ client/lib/app.js
@@ -1,5 +1,13 @@
 angular.module('kgapp',['angular-meteor','ui.router','ngMaterial']);
 
+function onReady() {
+  angular.bootstrap(document, ['kgapp']);
+}
+
+if (Meteor.isCordova)
+ angular.element(document).on("deviceready", onReady);
+else
+  angular.element(document).ready(onReady);
 
 var themeIcons = ['$mdIconProvider' , function ($mdIconProvider) {
  
